394f6355932be64159af172743f043cc04edbdc9
compu <careforme.dropout@gmail.com>
2025-04-08 11:18:56 +0900

    Add: vision_model_name and llm_name in .sh

--- run_projection_train_stage1.sh
+++ run_projection_train_stage1.sh
@@ -6,6 +6,8 @@ RUN_NAME="VD_Class_10_lr1e-4"
 accelerate launch --mixed_precision bf16 train_projection_stage1.py \
     --train_json /home/compu/DATA/CXR_VDQA/Train/formatted_VD_class.json \
     --image_root "/home/compu/DATA/NIH Chest X-rays_jpg" \
+    --vision_model_name "StanfordAIMI/XraySigLIP__vit-b-16-siglip-512__webli" \
+    --llm_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B" \
     --output_dir ./trained_projection_stage1/$RUN_NAME \
     --batch_size 2 \
     --num_epochs 10 \
