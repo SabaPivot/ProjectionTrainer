0018d33220f61fe77bf5baef77e1938242a2e58a
compu <careforme.dropout@gmail.com>
2025-04-08 11:05:40 +0900

    Delete: CheXagent.py

--- CheXagent.py
+++
@@ -1,6 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model = AutoModelForCausalLM.from_pretrained("StanfordAIMI/CheXagent-2-3b", trust_remote_code=True)
-tokenizer = AutoTokenizer.from_pretrained("StanfordAIMI/CheXagent-2-3b", trust_remote_code=True)
-
-print(model)
