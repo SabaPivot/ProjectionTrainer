11f3df9336e55646c48c72234b57d8bc7992a877
SabaPivot <careforme.dropout@gmail.com>
2025-04-10 12:02:38 +0900

    Refactor: Create Stage1 directory

--- Stage1/run_inference_stage1.sh
+++ Stage1/run_inference_stage1.sh
@@ -3,9 +3,10 @@
 python inference_stage1.py \
     --vision_model_name "StanfordAIMI/XraySigLIP__vit-l-16-siglip-384__webli" \
     --llm_name "google/gemma-3-1b-it" \
-    --projection_path "/home/compu/samuel/Siglip/trained_projection_stage1/VD_Class_10_lr5e-5_gemma3_vit-l-384/final_model" \
+    --projection_path "/home/compu/samuel/Siglip/trained_projection_stage1/VD_Class_20_lr5e-5_gemma3_vit-l-384/final_model" \
     --image_path "/home/compu/samuel/Siglip/images/001.png" \
-    --max_new_tokens 128 \
+    --max_new_tokens 256 \
     --temperature 0.6 \
     --top_p 0.9 \
     --repetition_penalty 1.4 \
+    --do_sample True \
